9e64745966a374f38e6cc62e56596b96d9a41497
cappielloraphael <raphaelcappiello39@gmail.com>
2024-06-18 12:37:45 +0000

    message

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ *
+ * Return: Always
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a ='a'; a <= 'z' ; a++)
+		putchar(a);
+	putchar('n');
+	return (0);
+}
