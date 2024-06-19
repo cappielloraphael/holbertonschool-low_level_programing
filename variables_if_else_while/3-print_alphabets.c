c1cd68d41174d79b8b56791a9d8ba1b265e2bafe
cappielloraphael <raphaelcappiello39@gmail.com>
2024-06-19 08:17:26 +0000

    message

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints letters
+ *
+ * Return: Always 
+ *
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabet ='a';alphabet <='z'; alphabet++)
+	{
+		for (alphabet ='A';alphabet <='Z';alphabet++)
+		{
+			putchar(alphabet)
+		}
+		putchar('\n');
+		return (0);
+
+	}
