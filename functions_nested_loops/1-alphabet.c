1f2913cd209e014bbf2179f650a54d87de48341d
cappielloraphael <raphaelcappiello39@gmail.com>
2024-06-21 08:50:05 +0000

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 int _putchar(char c);
+void print_alphabet(void);
 #endif
